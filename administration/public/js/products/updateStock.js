2e7e978c83ad76d08f017c0334d69f5ed2ea93b2
Davagulez <lezcadav@gmail.com>
2023-04-21 17:40:06 -0300

    ⚡️ finish logic in Stock

--- administration/public/js/products/updateStock.js
+++ administration/public/js/products/updateStock.js
@@ -8,17 +8,13 @@ const value3 = zone3.value
 const total = Number(document.querySelector('#total').innerHTML) 
 let sinAsignar = Number(document.querySelector('#unassigned').innerHTML)
 const error = document.querySelector('#error')
-console.log(total)
-console.log(sinAsignar)
-console.log(value1)
-console.log(value2)
-console.log(value3)
+const btn = document.querySelector('#update')
+
 
 function unAssigned(number1, number2, number3) {
     const sum = Number(number1) + Number(number2) + Number(number3)
-    console.log(sum)
     if (sum > total ) {
-        error.innerHTML += ' La cantidad por zona es mayor al stock disponible'
+        error.innerHTML = ` El total de las cantidades por zona es mayor al stock disponible, solo puede agregar ${sinAsignar}`
     } else {
         error.innerHTML = ''
     }
@@ -26,39 +22,48 @@ function unAssigned(number1, number2, number3) {
 
 zone1.addEventListener('change', (e)=>{
     const value = e.target.value
-    if (!(value1 != value)) {
-        unAssigned(value, value2, value3)
-        // zone1.removeAttribute('name')
-        console.log('removido')
-    } else {
-        unAssigned(value, value2, value3)
-        console.log('prueba exitosa')
-    }
+    unAssigned(value, value2, value3)
+    zone1.dataset.value = value
 })
 
 zone2.addEventListener('change', (e)=>{
     const value = e.target.value
-    if (!(value2 != value)) {
-        zone2.removeAttribute('name')
-        console.log('removido')
-    } else {
-        unAssigned(value1, value, value3)
-        console.log('prueba exitosa')
-    }
+    unAssigned(value1, value, value3)
+    zone2.dataset.value = value
 })
 
 zone3.addEventListener('change', (e)=>{
     const value = e.target.value
-    if (!(value3 != value)) {
-        zone3.removeAttribute('name')
-        console.log('removido')
-    } else {
-        unAssigned(value1, value2, value)
-        console.log('prueba exitosa')
-    }
+    unAssigned(value1, value2, value)
+    zone3.dataset.value = value
 })
 
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const send = new FormData(form)
+    const val1 = Number(send.get('stock1'))
+    const val2 = Number(send.get('stock2'))
+    const val3 = Number(send.get('stock3'))
+    const totalVal = val1 + val2 + val3
+    if ((val1 == value1) && (val2 == value2) && (val3 == value3)) {
+        error.innerHTML = 'No se ha hecho ninguna modificación'   
+    }
+    else if (totalVal > total) {
+        error.innerHTML = 'El total por zona no puede ser superior al stock disponible'
+    }
+    else if (val1 == value1) {
+        zone1.removeAttribute('name')
+    }
+    else if (val2 == value2) {
+        zone2.removeAttribute('name')
+    }
+    else if (val3 == value3) {
+        zone3.removeAttribute('name')
+    } else {
+        form.submit();
+    }
 
+})
 // const cancel = document.querySelector('#cancel')
 // form1.addEventListener('submit', (e)=>{
 //     e.preventDefault()
