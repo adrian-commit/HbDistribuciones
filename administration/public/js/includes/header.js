1e18893561c66437af801cc61ce80c8db35ecce8
Davagulez <lezcadav@gmail.com>
2023-05-24 16:16:37 -0300

    💄 Finish styles in header (mobile first)

--- administration/public/js/includes/header.js
+++ administration/public/js/includes/header.js
@@ -1,17 +1,29 @@
 const userImage = document.querySelector("#user-image");
 const userSection = document.querySelector("#user");
 const menu = document.querySelector('#header__menu');
+const menuDeploy = document.querySelector('#header__user');
+const cancelBtn = document.querySelector('#header__cancel');
 
-userImage.addEventListener("click", function() {
-  userSection.classList.toggle("visible");
-});
+// userImage.addEventListener("click", function() {
+//   userSection.classList.toggle("visible");
+// });
+
+// document.addEventListener("click", function(event) {
+//     if (!userSection.contains(event.target) && !userImage.contains(event.target)) {
+//       userSection.classList.remove("visible");
+//     }
+// });
+
+menu.addEventListener('click', function() {
+  menuDeploy.classList.toggle('header__menu--active');
+})
+
+cancelBtn.addEventListener('click', function () {
+  menuDeploy.classList.remove('header__menu--active');
+})
 
 document.addEventListener("click", function(event) {
-    if (!userSection.contains(event.target) && !userImage.contains(event.target)) {
-      userSection.classList.remove("visible");
+    if (!menuDeploy.contains(event.target) && !menu.contains(event.target) && !cancelBtn.contains(event.target)) {
+      menuDeploy.classList.remove('header__menu--active');
     }
-  });
-
-menu.addEventListener('click', ()=>{
-  
-})
+});
